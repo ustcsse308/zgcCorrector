0dff84146b00205179033824e0b1dbdad8ecd021
zzzgc <super_zgc@163.com>
2022-09-05 23:07:59 +0800

    update

--- src/dataset.py
+++ src/dataset.py
@@ -1,3 +1,7 @@
+from torch.utils.data import Dataset
+import tqdm
+import torch
+import random
 
 # 23个声母，24个韵母
 initials = 'b p m f d t n l ɡ k h j q x zh ch sh r z c s y w'.split(' ')
@@ -8,3 +12,136 @@ final2id = {final:i for i, final in enumerate(finals)}
 
 from pypinyin import lazy_pinyin, Style
 print(lazy_pinyin('绿色，',style = Style.TONE3))
+
+
+
+
+def getPictures(char, savePath, fontType, fontPath):
+    from PIL import Image, ImageFont, ImageDraw
+    image = Image.new('RGB', (250, 250), (255,255,255)) 
+    iwidth, iheight = image.size
+    font = ImageFont.truetype(fontPath+'/'+fontType, 60)
+    draw = ImageDraw.Draw(image)
+
+    fwidth, fheight = draw.textsize(char, font)
+
+    fontx = (iwidth - fwidth - font.getoffset(char)[0]) / 2
+    fonty = (iheight - fheight - font.getoffset(char)[1]) / 2
+
+    draw.text((fontx, fonty), char, 'black', font)
+    image.save(savePath+char+'.jpg') 
+
+class MultiModalDataset(Dataset):
+    def __init__(self, path, if_training=False, maxlen=128, glyphTokenizer=None, semanticTokenizer=None, speechTokenizer=None, detectorTokenizer=None):
+        self.maxlen = maxlen
+        self.glyphTokenizer = glyphTokenizer
+        self.semanticTokenizer = semanticTokenizer
+        self.speechTokenizer = speechTokenizer
+        self.detectorTokenizer = detectorTokenizer
+        self.corpus = []
+        with open(path, 'r') as f:
+            for line in f.readlines():
+                # src tar detect_label
+                line.strip().split('\t')
+                self.append(line)
+        self.if_training = if_training
+    
+    def __len__(self):
+        return len(self.corpus)
+    
+    def __getitem__(self, idx):
+        text_src, text_tar, detect_label = self.corpus[idx]
+        len_src = len(text_src)
+        
+        ret = {}
+        
+        # Glyph Tokenizer
+        glyphInput = self.getGlyphData(text_src)
+        ret['glyph_input_ids'] = glyphInput['input_ids']
+        ret['glyph_token_type_ids'] = glyphInput['token_type_ids']
+        ret['glyph_attention_mask'] = glyphInput['attention_mask']
+        
+        # Semantic Tokenizer
+        semanticInput = self.getSemanticData(text_src)
+        ret['semantic_input_ids'] = semanticInput['input_ids']
+        ret['semantic_token_type_ids'] = semanticInput['token_type_ids']
+        ret['semantic_attention_mask'] = semanticInput['attention_mask']
+        
+        # Speech Tokenizer
+        # TODO(@马承成): 完成时间(2022/09/10)
+        
+        
+        # Detector Tokenizer
+        detectorInput = self.getDetectorData(text_src)
+        ret['detector_input_ids'] = detectorInput['input_ids']
+        ret['detector_token_type_ids'] = detectorInput['token_type_ids']
+        ret['detector_attention_mask'] = detectorInput['attention_mask']
+        
+        
+        # label
+        # text_tar and detect_label
+        label = self.getLabelData(text_tar, detect_label)
+        ret['detect_label'] = label['detect_label']
+        ret['tar_label'] = label['tar_label']
+
+        return ret
+    
+    def getLabel(self, text_tar, detect_label):
+        tar = self.semanticTokenizer(
+            text_tar,
+            add_special_tokens=True,
+            return_tensors="pt",
+            return_token_type_ids=True,
+            return_attention_mask=True,
+            truncation=True,
+            padding=True,
+            max_length=self.maxlen
+        )
+        ret = {}
+        ret['tar_label'] = tar['input_ids']
+        detect_label = [int(label) for label in detect_label]
+        ret['detect_label'] = torch.tensor(detect_label, dtype=torch.long)
+        return ret
+
+    def getSpeechData(self, text_src):
+        # TODO(@马承成)：完成时间(2022/09/10)
+        pass
+    
+    def getGlyphData(self, text_src):
+        return self.glyphTokenizer(
+            text_src,
+            add_special_tokens=True,
+            return_tensors="pt",
+            return_token_type_ids=True,
+            return_attention_mask=True,
+            truncation=True,
+            padding=True,
+            max_length=self.maxlen
+        )
+    
+    def getSemanticData(self, text_src):
+        return self.semanticTokenizer(
+            text_src,
+            add_special_tokens=True,
+            return_tensors="pt",
+            return_token_type_ids=True,
+            return_attention_mask=True,
+            truncation=True,
+            padding=True,
+            max_length=self.maxlen
+        )
+
+    def getDetectorData(self, text_src):
+        return self.detectorTokenizer(
+            text_src,
+            add_special_tokens=True,
+            return_tensors="pt",
+            return_token_type_ids=True,
+            return_attention_mask=True,
+            truncation=True,
+            padding=True,
+            max_length=self.maxlen
+        )
+        
+if __name__ == '__main__':
+    pass
--- src/modelling.py
+++ src/modelling.py
@@ -13,22 +13,74 @@ class DetectModel(nn.Module):
             self.bert = AutoModel.from_pretrained(DetectModelPath)
         else:
             self.bert = ElectraForPreTraining.from_pretrained(DetectModelPath)
-
         self.type = type
         self.classifier = nn.Linear(self.bert.config.hidden_size, 1)
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, **kwarg):
         output = self.bert(input_ids, token_type_ids, attention_mask)
-        print(output)
         if self.type == 'bert':
             output = self.classifier(output.last_hidden_state)
             s_output = output.size()
             output = output.view(s_output[0], -1)
         else:
             output = output.logits
+        # output:[batch, seq_len]
         return output
 
+class SemanticModel(nn.Module):
+    def __init__(self, SemanticModelPath, type='electra'):
+        super(SemanticModel, self).__init__()
+        if type == 'bert':
+            self.bert = AutoModel.from_pretrained(SemanticModel)
+        else:
+            self.bert = ElectraForPreTraining.from_pretrained(SemanticModel)
+        self.type = type
 
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, **kwarg):
+        output = self.bert(input_ids, token_type_ids, attention_mask)
+        if self.type == 'bert':
+            output = output.last_hidden_state
+        else:
+            output = output.logits
+        # output:[batch, seq_len, hid]
+        return output
+
+# TODO(@马承成): 完成时间2022/09/10
+class SpeechModel(nn.Module):
+    def __init__(self, SpeechModelPath, type='bert'):
+        super(SpeechModel, self).__init__()
+        pass
+    
+    
+# TODO(@马承成): 完成时间2022/09/10
+class GlyphModel(nn.Module):
+    def __init__(self, GlyphModelPath, type='bert'):
+        super(GlyphModel, self).__init__()
+        pass
+    
+    
+
+class Corrector(nn.Module):
+    def __init__(self, SemanticModelPath, SpeechModelPath, GlyphModelPath, DetectModelPath, typeSemantic='bert', typeSpeech='', typeGlyph='bert', typeDetect='electra'):
+        self.GlyphModel = GlyphModel(GlyphModelPath, typeGlyph)
+        self.DetectModel = DetectModel(DetectModelPath, typeDetect)
+        self.SpeechModel = SpeechModel(SpeechModelPath, typeSpeech)
+        self.SemanticModel = SemanticModel(SemanticModelPath, typeSemantic)
+    
+    def getKL4AttentionMatrix(self, l1, l2, l3):
+        # TODO(@zzzgc)
+        pass
+
+    def forward(self, x):
+        logits4Semantic = self.GlyphModel(input_ids=x['semantic_input_ids'], token_type_ids=x['semantic_input_ids'], attention_mask=x['semantic_input_ids'])
+        logits4Glyph = self.GlyphModel(input_ids=x['glyph_input_ids'], token_type_ids=x['glyph_token_type_ids'], attention_mask=x['glyph_attention_mask'])
+        # TODO(@马承成)
+        logits4Speech = self.SpeechModel(input_ids=x['semantic_input_ids'], token_type_ids=x['semantic_input_ids'], attention_mask=x['semantic_input_ids'])
+        logists4Detect = self.DetectModel(input_ids=x['detector_input_ids'], token_type_ids=x['detector_token_type_ids'], attention_mask=x['detector_attention_mask'])
+        KLoss = self.getKL4AttentionMatrix(logits4Semantic, logits4Glyph, logits4Speech)
+        # TODO(@zzzgc)
+        
+    
 def test_detect():
     from transformers import ElectraForPreTraining, ElectraTokenizerFast
     import torch
